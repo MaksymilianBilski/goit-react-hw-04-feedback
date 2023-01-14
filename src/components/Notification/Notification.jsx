d1163c625ef0a85c9b0cd837ec624b67f728bbdf
Maksymilian Bilski <biger9@o2.pl>
2023-01-14 19:05:05 +0100

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,6 +2,6 @@ import { Component } from 'react';
 
 export class Notification extends Component {
   render() {
-    return <h5>No feedbackk given</h5>;
+    return <h5>No feedback given</h5>;
   }
 }
