02267589bdb6859243a649142571fd61dc0f48ae
Maksymilian Bilski <biger9@o2.pl>
2023-01-16 19:57:32 +0100

    custom component hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,33 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
-import { FeedbackContext } from './context/FeedbackContext/FeedbackContext';
+import { Notification } from './Notification/Notification';
+import { useFeedbackContext } from './context/FeedbackContext/FeedbackContext';
 
 export const App = () => {
-  const [stat, setStat] = useState({ good: 0, neutral: 0, bad: 0 });
-  let total = 0;
-  const options = Object.keys(stat);
-  const countTotalFeedback = () => {
-    if (Object.values(stat).some(val => val > 0)) {
-      total = Object.values(stat).reduce((pV, num) => {
-        return (pV += num);
-      }, 0);
-      return total;
-    }
-  };
-  const countPositiveFeedbackPercentage = () => {
-    const sum = Object.values(stat).reduce((pV, num) => {
-      return (pV += num);
-    }, 0);
-    const positiveSum = (stat.good / sum).toFixed(2);
-    return Number(positiveSum);
-  };
-
-  const add = evt => {
-    setStat(prevState => {
-      return { ...prevState, [evt]: prevState[evt] + 1 };
-    });
-  };
-  const buttonClick = evt => {
-    const btnName = evt.target.textContent;
-    add(btnName);
-  };
-
+  const {
+    buttonClick,
+    options,
+    stat,
+    countPositiveFeedbackPercentage,
+    countTotalFeedback,
+  } = useFeedbackContext();
   return (
     <div>
-      <FeedbackContext.Provider
-        value={{
-          add,
-          buttonClick,
-          countPositiveFeedbackPercentage,
-          countTotalFeedback,
-          stat,
-          options,
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions buttonClick={buttonClick} options={options} />
-        </Section>
-        <Section title="Statistics">
+      <Section title="Please leave feedback">
+        <FeedbackOptions buttonClick={buttonClick} options={options} />
+      </Section>
+      <Section title="Statistics">
+        {Object.values(stat).some(el => el > 0) ? (
           <Statistics
             stats={stat}
             positive={countPositiveFeedbackPercentage()}
             total={countTotalFeedback()}
           />
-        </Section>
-      </FeedbackContext.Provider>
+        ) : (
+          <Notification />
+        )}
+      </Section>
     </div>
   );
-};
-FeedbackContext.Provider.propTypes = {
-  add: PropTypes.func,
-  buttonClick: PropTypes.func,
-  countPositiveFeedbackPercentage: PropTypes.func,
-  countTotalFeedback: PropTypes.func,
-  stat: PropTypes.object,
-  options: PropTypes.array,
 };
--- src/components/context/FeedbackContext/FeedbackContext.jsx
+++ src/components/context/FeedbackContext/FeedbackContext.jsx
@@ -1,4 +1,59 @@
-import { useContext, createContext } from 'react';
+import { useContext, createContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const FeedbackContext = createContext();
 export const useFeedbackContext = () => useContext(FeedbackContext);
+
+const FeedbackContextProvider = ({ children }) => {
+  const [stat, setStat] = useState({ good: 0, neutral: 0, bad: 0 });
+  const options = Object.keys(stat);
+  const countTotalFeedback = () => {
+    if (Object.values(stat).some(val => val > 0)) {
+      let total = Object.values(stat).reduce((pV, num) => {
+        return (pV += num);
+      }, 0);
+      return total;
+    }
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const sum = Object.values(stat).reduce((pV, num) => {
+      return (pV += num);
+    }, 0);
+    const positiveSum = (stat.good / sum).toFixed(2);
+    return Number(positiveSum);
+  };
+
+  const add = evt => {
+    setStat(prevState => {
+      return { ...prevState, [evt]: prevState[evt] + 1 };
+    });
+  };
+  const buttonClick = evt => {
+    const btnName = evt.target.textContent;
+    add(btnName);
+  };
+  return (
+    <FeedbackContext.Provider
+      value={{
+        buttonClick,
+        add,
+        countPositiveFeedbackPercentage,
+        countTotalFeedback,
+        options,
+        stat,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+FeedbackContextProvider.propTypes = {
+  add: PropTypes.func,
+  buttonClick: PropTypes.func,
+  countPositiveFeedbackPercentage: PropTypes.func,
+  countTotalFeedback: PropTypes.func,
+  stat: PropTypes.object,
+  options: PropTypes.array,
+};
+
+export default FeedbackContextProvider;
